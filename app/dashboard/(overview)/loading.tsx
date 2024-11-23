f7bb0dc55eb5943bd069cd685b99d334ddc6aad4
khoatdt <88079474+khoatdt@users.noreply.github.com>
2024-11-23 15:23:21 +0700

    update project

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import styles from '@/app/ui/home.module.css';
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+ return <div ><DashboardSkeleton/></div>;
+}
