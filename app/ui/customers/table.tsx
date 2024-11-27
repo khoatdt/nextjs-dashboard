e7accfacbdbbf7695ca483c3eec8258fc932385f
khoatdt <88079474+khoatdt@users.noreply.github.com>
2024-11-27 16:50:36 +0700

    update create, edit form & delete invoice

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,9 +1,6 @@
-import Image from 'next/image';
-import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import Image from "next/image";
+import Search from "@/app/ui/search";
+import { FormattedCustomersTable } from "@/app/lib/definitions";
 
 export default async function CustomersTable({
   customers,
@@ -12,9 +9,7 @@ export default async function CustomersTable({
 }) {
   return (
     <div className="w-full">
-      <h1 className={` mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
+      <h1 className={` mb-8 text-xl md:text-2xl`}>Customers</h1>
       <Search placeholder="Search customers..." />
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
